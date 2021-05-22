6188e65d6de912afd23413874f3f505eccd37240
Shergin Lev <work.mail.lev@gmail.com>
2021-05-22 19:01:21 +0900

    start

---
+++ news/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import News
+
+
+def index(request):
+    news = News.objects.all()
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
